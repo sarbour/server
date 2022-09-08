f49a4156fcb4cdb27dc7a34ddafecc64ae58c0d2
Spencer Arbour <spen.arbour@gmail.com>
2022-09-08 13:16:02 -0700

    Sets up server connection port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let port = "127.0.0.1:8080".to_string();
+    let server = Server::new(port);
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr
+        }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr)
+    }
 }
