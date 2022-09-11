73e6f742ac3db8462dcd34130880ddbb0e87112a
Spencer Arbour <spen.arbour@gmail.com>
2022-09-11 13:49:54 -0700

    add RequestParse error handling

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,49 @@
 use super::method::Method;
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: Method,
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Result as FmtResult, Display, Formatter, Debug };
+pub struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
         }
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
-use std::net::TcpListener;
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::io::Read;
+use std::net::TcpListener;
 pub struct Server {
     addr: String,
 }
@@ -23,6 +25,11 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse a request: {}", e)
+                            }
                         },
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
