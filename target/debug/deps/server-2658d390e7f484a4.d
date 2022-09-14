ceca5834e01de5d2f174de68ae7347da972f1c1d
Spencer Arbour <spen.arbour@gmail.com>
2022-09-14 10:34:06 -0700

    Add response framework

--- target/debug/deps/server-2658d390e7f484a4.d
+++ target/debug/deps/server-2658d390e7f484a4.d
@@ -1,10 +1,11 @@
-/Users/spenny/Documents/RustProjects/server/target/debug/deps/server-2658d390e7f484a4.rmeta: src/main.rs src/http/mod.rs src/http/method.rs src/http/query_string.rs src/http/request.rs src/server.rs
+/Users/spenny/Documents/RustProjects/server/target/debug/deps/server-2658d390e7f484a4.rmeta: src/main.rs src/http/mod.rs src/http/method.rs src/http/query_string.rs src/http/request.rs src/http/response.rs src/server.rs
 
-/Users/spenny/Documents/RustProjects/server/target/debug/deps/server-2658d390e7f484a4.d: src/main.rs src/http/mod.rs src/http/method.rs src/http/query_string.rs src/http/request.rs src/server.rs
+/Users/spenny/Documents/RustProjects/server/target/debug/deps/server-2658d390e7f484a4.d: src/main.rs src/http/mod.rs src/http/method.rs src/http/query_string.rs src/http/request.rs src/http/response.rs src/server.rs
 
 src/main.rs:
 src/http/mod.rs:
 src/http/method.rs:
 src/http/query_string.rs:
 src/http/request.rs:
+src/http/response.rs:
 src/server.rs:
